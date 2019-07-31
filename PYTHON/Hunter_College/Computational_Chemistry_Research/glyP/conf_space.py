3beada4b59614077872da500f6e6234baa00e485
Cesar Rene Pabon Bernal <cesarpbn1@gmail.com>
2019-07-31 13:41:02 -0400

    up

--- PYTHON/Hunter_College/Computational_Chemistry_Research/glyP/conf_space.py
+++ PYTHON/Hunter_College/Computational_Chemistry_Research/glyP/conf_space.py
@@ -2,6 +2,7 @@ import numpy as np
 import os
 from conformer import *
 from copy import copy as cp
+from scipy import interpolate
 
 
 class Space(list):
@@ -16,15 +17,25 @@ class Space(list):
     _temp = 298.15
     _kT=0.0019872036*_temp
     _Ha2kcal=627.5095
+    w_incr = 0.5
 
-    def __init__(self, molecule):
-
+    def __init__(self, molecule, ir_resolution=1):
+        
+        self.ir_resolution = ir_resolution #check whether float will work here
+        
         for (root, dirs, files) in os.walk('./'+molecule):
             for dirname in dirs:
                 print dirname
                 #oldername = os.path.basename(dirpath)
                 if dirname == 'experimental':
-                    self.expIR = np.genfromtxt(molecule+'/'+dirname+'/exp.dat')
+                    self.ir_resolution = np.genfromtxt(molecule+'/'+dirname+'/exp.dat')
+                        if self.ir_resultion[0]=="w_incr":
+                        w_incr=float(line[1])
+                        incr = 0.1*w_incr
+                        grid_old = numpy.arange(0,len(integrand))*w_incr
+                        grid_new = numpy.arange(grid_old[0],grid_old[-1]+incr,incr)
+                        spl = interpolate.splrep(grid_old,integrand.T[1],k=3,s=0)
+                        integrand_dense = interpolate.splev(grid_new,spl,der=0)
                 for ifiles in os.walk(molecule+'/'+dirname):
                     for filename in ifiles[2]:
                         if filename.endswith('.log'):
@@ -39,11 +50,11 @@ class Space(list):
             print "%20s%20.2f%20.2f%20.2f\n" %(conf._id, conf.E*self._Ha2kcal, conf.H*self._Ha2kcal, conf.F*self._Ha2kcal),
         return ''
 
-    def gaussian_broadening(self, broaden=5):
+    def gaussian_broadening(self, broaden=5, self._ir_resolution):
 
         ''' Performs gaussian broadening for the set''' 
 
-        for conf in self: conf.gaussian_broadening(broaden)
+        for conf in self: conf.gaussian_broadening(broaden, resolution=self._ir_resolution)
 
     def reference_to_zero(self, energy_function='E'):
 
